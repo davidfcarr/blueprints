112f2bb204898aef57a29f169041a60a2b518f40
davidfcarr <david@carrcommunications.com>
2024-10-14 20:48:49 -0400

    permalinks

--- test-plugin/test-plugin.php
+++ test-plugin/test-plugin.php
@@ -466,3 +466,5 @@ function playground_test() {
 
     //global $wp_rewrite; $wp_rewrite->set_permalink_structure('/%postname%/'); $wp_rewrite->flush_rules();
 }
+
+add_action( 'after_setup_theme', function() { global $wp_rewrite; $wp_rewrite->set_permalink_structure('/%postname%/'); $wp_rewrite->flush_rules(); } );
